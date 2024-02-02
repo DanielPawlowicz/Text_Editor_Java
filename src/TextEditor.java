a086711edc48df3ccd7a74c0a137bf9f803f3d0f
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-02 11:16:58 +0100

    TextEditor class constructor

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,3 +1,4 @@
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,6 +6,14 @@ import javax.swing.JFrame;
 
 public class TextEditor extends JFrame implements ActionListener{
 
+	TextEditor(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Text Editor");
+		this.setSize(500,500);
+		this.setLayout(new FlowLayout());
+		this.setVisible(true);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
