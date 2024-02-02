cc2ef65396bb8f338f1f8d3844bac2fbfc81a761
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-02 13:03:04 +0100

    Changing font family and color

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
@@ -16,6 +22,9 @@ public class TextEditor extends JFrame implements ActionListener{
 	JTextArea textArea;
 	JScrollPane scroll;
 	JSpinner fontSizeSpinner;
+	JLabel fontLabel;
+	JButton fontColorButton;
+	JComboBox fontBox;
 	
 	TextEditor(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,9 +59,21 @@ public class TextEditor extends JFrame implements ActionListener{
 			}
 		});
 		
+		fontLabel = new JLabel("Font: ");
 		
+		fontColorButton = new JButton("Color");
+		fontColorButton.addActionListener(this);
 		
+		String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		
+		fontBox = new JComboBox(fonts);
+		fontBox.addActionListener(this);
+		fontBox.setSelectedItem("Arial");
+		
+		this.add(fontLabel);
 		this.add(fontSizeSpinner);
+		this.add(fontBox);
+		this.add(fontColorButton);
 		this.add(scroll);
 //		this.add(textArea);
 		this.setVisible(true);
@@ -60,7 +81,18 @@ public class TextEditor extends JFrame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		if(e.getSource() == fontColorButton) {
+			JColorChooser colorChooser = new JColorChooser();
+			
+			Color color = colorChooser.showDialog(null, "Choose a color", Color.black);
+			
+			textArea.setForeground(color);
+		}
+		
+		if(e.getSource()==fontBox) {
+			textArea.setFont(new Font( (String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+		}
 		
 	}
 
